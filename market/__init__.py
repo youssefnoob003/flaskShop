22759c145c827dab3f76b49d46e375e0d6e699c4
siBouZitoun <gumball.daruine@gmail.com>
2024-01-29 23:06:13 +0100

    added register form

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '8ea0aa331a0561ff54052f46'
 db = SQLAlchemy(app)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template
 from market.models import Item
-
+from market.forms import RegisterForm
 
 @app.route("/")
 @app.route("/home")
@@ -14,3 +14,9 @@ def market():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
+
+
+@app.route("/register")
+def register():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
